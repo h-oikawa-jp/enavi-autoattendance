f3f0f45ffbb8cc735e04be6cd3f952f6a607618f
oikawa <oikawa@techfun.jp>
2017-07-04 00:11:40 +0900

    Windows用バッチ：ログ出力を追加

--- workStart.bat
+++ workStart.bat
@@ -2,11 +2,19 @@
 
 cd %~dp0
 
-echo "勤怠入力を実行します。中止する場合はウィンドウを閉じてください。"
-timeout 10
+rem ログフォルダ/ファイル名を生成
+if not exist .log (mkdir .log)
+set YMD=%date:~0,4%%date:~5,2%%date:~8,2%
+set LOGFILE=.log\%~n0_%YMD%.log
 
-npm run enavi -- workStart
+
+echo 勤怠入力を実行します。中止する場合はウィンドウを閉じてください。
+timeout 5
+
+
+call npm run enavi -- workStart -e %USERNAME% >>%LOGFILE%
+type %LOGFILE%
+
 
 pause
-
-exit /B
+exit /b %ecode%
